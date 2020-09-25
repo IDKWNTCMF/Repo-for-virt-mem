0b14adc90800859b502338da1d01872e6b1c14d0
IDKWNTCMF <artemilyanich@mail.ru>
2020-09-25 15:10:24 +0300

    Add first version of LRU algorithm

--- src/main/kotlin/VirtualMemory.kt
+++ src/main/kotlin/VirtualMemory.kt
@@ -1,11 +1,11 @@
 import java.util.*
 
-data class Replacement(val cellNumber: Int, val replacedValue: Int, val newValue: Int)
+data class Replacement(val frameNumber: Int, val replacedValue: Int, val newValue: Int)
 
 val m = 3
 val n = 10
 
-fun getNumberOfReplacedCell(curReplacement: Int) = if (curReplacement % m == 0) m else curReplacement % m
+fun getNumberOfReplacedFrame(curReplacement: Int) = if (curReplacement % m == 0) m else curReplacement % m
 
 fun FIFO(appeals: List<Int>): Pair<Int, List<Replacement>> {
     val memory: Queue<Int> = LinkedList<Int>()
@@ -14,16 +14,45 @@ fun FIFO(appeals: List<Int>): Pair<Int, List<Replacement>> {
     for (curPage in appeals) {
         if (memory.contains(curPage)) {
             replacements.add(Replacement(0, curPage, curPage)) // 0 means that the needed page is in memory
-            continue
         } else {
+            numberOfReplacements++
             var replacedValue = 0
             if (memory.size == m) {
                 replacedValue = memory.element()
                 memory.remove()
             }
             memory.add(curPage)
+            replacements.add(Replacement(getNumberOfReplacedFrame(numberOfReplacements), replacedValue, curPage))
+        }
+    }
+    return Pair(numberOfReplacements, replacements)
+}
+
+fun LRU(appeals: List<Int>): Pair<Int, List<Replacement>> {
+    val memory = mutableListOf<Int>()
+    val pretendentsForReplacement = mutableListOf<Int>()
+    var numberOfReplacements = 0
+    val replacements = mutableListOf<Replacement>()
+    for (curPage in appeals) {
+        if (memory.contains(curPage)) {
+            replacements.add(Replacement(0, curPage, curPage)) // 0 means that the needed page is in memory
+            pretendentsForReplacement.remove(curPage)
+            pretendentsForReplacement.add(curPage)
+        } else {
             numberOfReplacements++
-            replacements.add(Replacement(getNumberOfReplacedCell(numberOfReplacements), replacedValue, curPage))
+            var replacedValue = 0
+            var replacedFrame = getNumberOfReplacedFrame(numberOfReplacements)
+            if (memory.size == m) {
+                replacedValue = pretendentsForReplacement[0]
+                pretendentsForReplacement.remove(pretendentsForReplacement[0])
+                replacedFrame = memory.indexOf(replacedValue) + 1
+                pretendentsForReplacement.add(curPage)
+                memory[replacedFrame - 1] = curPage
+            } else {
+                memory.add(curPage)
+                pretendentsForReplacement.add(curPage)
+            }
+            replacements.add(Replacement(replacedFrame, replacedValue, curPage))
         }
     }
     return Pair(numberOfReplacements, replacements)
@@ -34,14 +63,28 @@ fun main(args: Array<String>) {
     val FIFOresult = FIFO(appeals)
     println("FIFO: ${FIFOresult.first} replacements")
     for (replacement in FIFOresult.second) {
-        if (replacement.cellNumber == 0) {
+        if (replacement.frameNumber == 0) {
             println("${replacement.newValue}: The needed page is in memory")
             continue
         }
         if (replacement.replacedValue == 0){
-            println("Add ${replacement.newValue} in ${replacement.cellNumber} cell")
+            println("Add ${replacement.newValue} in ${replacement.frameNumber} cell")
         } else {
-            println("Replace ${replacement.replacedValue} with ${replacement.newValue} in ${replacement.cellNumber} cell")
+            println("Replace ${replacement.replacedValue} with ${replacement.newValue} in ${replacement.frameNumber} cell")
+        }
+    }
+    println("")
+    val LRUresult = LRU(appeals)
+    println("LRU: ${LRUresult.first} replacements")
+    for (replacement in LRUresult.second) {
+        if (replacement.frameNumber == 0) {
+            println("${replacement.newValue}: The needed page is in memory")
+            continue
+        }
+        if (replacement.replacedValue == 0){
+            println("Add ${replacement.newValue} in ${replacement.frameNumber} cell")
+        } else {
+            println("Replace ${replacement.replacedValue} with ${replacement.newValue} in ${replacement.frameNumber} cell")
         }
     }
 }
