4464a0f27fd2216e8da2d238dda77f9024723df5
IDKWNTCMF <artemilyanich@mail.ru>
2020-09-26 12:54:56 +0300

    Upgrade realization of algorithms

--- src/main/kotlin/VirtualMemory.kt
+++ src/main/kotlin/VirtualMemory.kt
@@ -1,5 +1,3 @@
-import java.util.*
-
 data class Replacement(val frameNumber: Int, val replacedValue: Int, val newValue: Int)
 
 val m = 3
@@ -7,147 +5,94 @@ val n = 5
 
 fun getNumberOfReplacedFrame(curReplacement: Int) = if (curReplacement % m == 0) m else curReplacement % m
 
-fun FIFO(appeals: List<Int>): Pair<Int, List<Replacement>> {
-    val memory: Queue<Int> = LinkedList<Int>()
-    var numberOfReplacements = 0
-    val replacements = mutableListOf<Replacement>()
-    for (curPage in appeals) {
-        if (memory.contains(curPage)) {
-            replacements.add(Replacement(0, curPage, curPage)) // 0 means that the needed page is in memory
-        } else {
-            numberOfReplacements++
-            var replacedValue = 0
-            if (memory.size == m) {
-                replacedValue = memory.element()
-                memory.remove()
-            }
-            memory.add(curPage)
-            replacements.add(Replacement(getNumberOfReplacedFrame(numberOfReplacements), replacedValue, curPage))
+fun findReplacedFrameFIFO(indexOfElementInMemory: List<Int>): Int {
+    var replacedFrame = 0
+    for (curFrame in 0 until m) {
+        if (indexOfElementInMemory[curFrame] < indexOfElementInMemory[replacedFrame]) {
+            replacedFrame = curFrame
         }
     }
-    return Pair(numberOfReplacements, replacements)
+    return replacedFrame + 1
 }
 
-fun LRU(appeals: List<Int>): Pair<Int, List<Replacement>> {
+fun findReplacedFrameLRU(processedPrefixSize: Int, appeals: List<Int>, memory: List<Int>): Int {
+    val lastAppeal = MutableList(n + 1) {-1}
+    for (index in 0 until processedPrefixSize) {
+        lastAppeal[appeals[index]] = index
+    }
+    var replacedFrame = 0
+    for (curFrame in 0 until m) {
+        if (lastAppeal[memory[curFrame]] < lastAppeal[memory[replacedFrame]]) {
+            replacedFrame = curFrame
+        }
+    }
+    return replacedFrame + 1
+}
+
+fun findReplacedFrameOPT(processedPrefixSize: Int, appeals: List<Int>, memory: List<Int>): Int {
+    val nextAppeal = MutableList(n + 1) {appeals.size}
+    for (index in processedPrefixSize + 1 until appeals.size) {
+        if (nextAppeal[appeals[index]] == appeals.size) {
+            nextAppeal[appeals[index]] = index
+        }
+    }
+    var replacedFrame = 0
+    for (curFrame in 0 until m) {
+        if (nextAppeal[memory[curFrame]] > nextAppeal[memory[replacedFrame]]) {
+            replacedFrame = curFrame
+        }
+    }
+    return replacedFrame + 1
+}
+
+fun algorithm(whatAlgorithm: String, appeals: List<Int>): Pair<Int, List<Replacement>>{
     val memory = mutableListOf<Int>()
-    val pretendentsForReplacement = mutableListOf<Int>()
+    val indexOfElementInMemory = mutableListOf<Int>()
     var numberOfReplacements = 0
     val replacements = mutableListOf<Replacement>()
-    for (curPage in appeals) {
-        if (memory.contains(curPage)) {
-            replacements.add(Replacement(0, curPage, curPage)) // 0 means that the needed page is in memory
-            pretendentsForReplacement.remove(curPage)
-            pretendentsForReplacement.add(curPage)
-        } else {
-            numberOfReplacements++
-            var replacedValue = 0
-            var replacedFrame = getNumberOfReplacedFrame(numberOfReplacements)
-            if (memory.size == m) {
-                replacedValue = pretendentsForReplacement[0]
-                pretendentsForReplacement.remove(pretendentsForReplacement[0])
-                replacedFrame = memory.indexOf(replacedValue) + 1
-                pretendentsForReplacement.add(curPage)
-                memory[replacedFrame - 1] = curPage
-            } else {
-                memory.add(curPage)
-                pretendentsForReplacement.add(curPage)
-            }
-            replacements.add(Replacement(replacedFrame, replacedValue, curPage))
-        }
-    }
-    return Pair(numberOfReplacements, replacements)
-}
-
-fun getNextEntry(appeals: List<Int>): List<Int> {
-    val curLastEntry = MutableList(n + 1) {-1}
-    val nextEntry = MutableList(appeals.size) {0}
-    for ((curIndex, curAppeal) in appeals.withIndex()) {
-        nextEntry[curIndex] = appeals.size + curIndex // this secret instrument will help us later
-        if (curLastEntry[curAppeal] != -1) {
-            nextEntry[curLastEntry[curAppeal]] = curIndex
-        }
-        curLastEntry[curAppeal] = curIndex
-    }
-    return nextEntry
-}
-
-fun OPT(appeals: List<Int>): Pair<Int, List<Replacement>> {
-    val memory = mutableListOf<Int>()
-    val pretendentsForReplacement = mutableListOf<Int>()
-    var numberOfReplacements = 0
-    val replacements = mutableListOf<Replacement>()
-    val nextEntry = getNextEntry(appeals)
     for ((curIndex, curPage) in appeals.withIndex()) {
         if (memory.contains(curPage)) {
-            replacements.add(Replacement(0, curPage, curPage)) // 0 means that the needed page is in memory
-            pretendentsForReplacement.remove(curIndex)
-            pretendentsForReplacement.add(nextEntry[curIndex])
-        } else {
-            numberOfReplacements++
-            var replacedValue = 0
-            var replacedFrame = getNumberOfReplacedFrame(numberOfReplacements)
-            if (memory.size == m) {
-                val curPretendent: Int = pretendentsForReplacement.max()!!
-                if (curPretendent >= appeals.size) {
-                    replacedValue = appeals[curPretendent - appeals.size]
-                } else {
-                    replacedValue = appeals[curPretendent]
-                }
-                pretendentsForReplacement.remove(curPretendent)
-                replacedFrame = memory.indexOf(replacedValue) + 1
-                pretendentsForReplacement.add(nextEntry[curIndex])
-                memory[replacedFrame - 1] = curPage
-            } else {
-                memory.add(curPage)
-                pretendentsForReplacement.add(nextEntry[curIndex])
-            }
-            replacements.add(Replacement(replacedFrame, replacedValue, curPage))
+            replacements.add(Replacement(0, curPage, curPage))
+            continue
         }
+        numberOfReplacements++
+        if (memory.size < m) {
+            memory.add(curPage)
+            indexOfElementInMemory.add(curIndex)
+            replacements.add(Replacement(getNumberOfReplacedFrame(numberOfReplacements), 0, curPage))
+            continue
+        }
+        val replacedFrame = when (whatAlgorithm) {
+            "FIFO" -> findReplacedFrameFIFO(indexOfElementInMemory)
+            "LRU" -> findReplacedFrameLRU(curIndex, appeals, memory)
+            else -> findReplacedFrameOPT(curIndex, appeals, memory) // this branch is for an OPT algorithm
+        }
+        replacements.add(Replacement(replacedFrame, memory[replacedFrame - 1], curPage))
+        memory[replacedFrame - 1] = curPage
+        indexOfElementInMemory[replacedFrame - 1] = curIndex
     }
     return Pair(numberOfReplacements, replacements)
+}
+
+fun outputTheResult(whatAlgorithm: String, resultOfAlgorithm: Pair<Int, List<Replacement>>) {
+    println("$whatAlgorithm: ${resultOfAlgorithm.first} replacements")
+    for (replacement in resultOfAlgorithm.second) {
+        if (replacement.frameNumber == 0) {
+            println("${replacement.newValue}: The needed page is in memory")
+            continue
+        }
+        if (replacement.replacedValue == 0) {
+            println("Add ${replacement.newValue} in ${replacement.frameNumber} frame")
+        } else {
+            println("Replace ${replacement.replacedValue} with ${replacement.newValue} in ${replacement.frameNumber} frame")
+        }
+    }
+    println("<------The end of $whatAlgorithm output------>")
 }
 
 fun main(args: Array<String>) {
     val appeals = listOf(1, 2, 3, 4, 5, 1, 3, 5, 2, 1)
-    val FIFOresult = FIFO(appeals)
-    println("FIFO: ${FIFOresult.first} replacements")
-    for (replacement in FIFOresult.second) {
-        if (replacement.frameNumber == 0) {
-            println("${replacement.newValue}: The needed page is in memory")
-            continue
-        }
-        if (replacement.replacedValue == 0) {
-            println("Add ${replacement.newValue} in ${replacement.frameNumber} frame")
-        } else {
-            println("Replace ${replacement.replacedValue} with ${replacement.newValue} in ${replacement.frameNumber} frame")
-        }
-    }
-    println("")
-    val LRUresult = LRU(appeals)
-    println("LRU: ${LRUresult.first} replacements")
-    for (replacement in LRUresult.second) {
-        if (replacement.frameNumber == 0) {
-            println("${replacement.newValue}: The needed page is in memory")
-            continue
-        }
-        if (replacement.replacedValue == 0) {
-            println("Add ${replacement.newValue} in ${replacement.frameNumber} frame")
-        } else {
-            println("Replace ${replacement.replacedValue} with ${replacement.newValue} in ${replacement.frameNumber} frame")
-        }
-    }
-    println("")
-    val OPTresult = OPT(appeals)
-    println("OPT: ${OPTresult.first} replacements")
-    for (replacement in OPTresult.second) {
-        if (replacement.frameNumber == 0) {
-            println("${replacement.newValue}: The needed page is in memory")
-            continue
-        }
-        if (replacement.replacedValue == 0) {
-            println("Add ${replacement.newValue} in ${replacement.frameNumber} frame")
-        } else {
-            println("Replace ${replacement.replacedValue} with ${replacement.newValue} in ${replacement.frameNumber} frame")
-        }
-    }
+    outputTheResult("FIFO", algorithm("FIFO", appeals))
+    outputTheResult("LRU", algorithm("LRU", appeals))
+    outputTheResult("OPT", algorithm("OPT", appeals))
 }
